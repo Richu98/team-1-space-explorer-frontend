50e70c83418c6dc60ca8939be70be5bee31138ea
Your Name <ChrisMassey03>
2020-06-01 21:53:58 +0530

    Initiated The App

---
+++ src/Components/Begin/Begin.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './Begin.css';
+
+
+
+class Begin extends Component {
+
+  render() {
+
+    return (
+      <div className="Begin">
+
+        {/* -----HEADER----- */}
+
+        <div className="header">
+
+          <h1>Lost In Space....</h1>
+          <h2>Let's get going... </h2>
+
+        </div>
+
+
+
+      </div>
+    );
+  }
+}
+
+export default Begin;
---
+++ src/Components/TeamInfo/TeamInfo.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './TeamInfo.css';
+
+
+class TeamInfo extends Component{
+
+
+    render(){
+
+        return(
+            <div className="TeamInfo">
+               
+               {/* -----FORM----- */}
+               
+                
+
+            </div>
+        );
+
+
+    }
+}
+
+export default TeamInfo;
