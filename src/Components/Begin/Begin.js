5ad0cee8e13ba518773ae687dce8426dc18ec2da
Your Name <ChrisMassey03>
2020-06-03 16:58:29 +0530

    updated the files

--- src/Components/Begin/Begin.js
+++ src/Components/Begin/Begin.js
@@ -5,50 +5,49 @@ import './Begin.css';
 
 class Begin extends Component {
 
-state = {
-  id: "",
-  teamName: "",
-  destination: "",
-  members: [],
-  spaceShipName: "",
-  mission: ""
-}
+  state = {
+    teamName: "",
+    destination: "",
+    members: [],
+    spaceShipName: "",
+    mission: ""
+  }
 
-handlechange = (e) => {
+  handlechange = (e) => {
     this.setState({
-    [e.target.id] : e.target.value
-  });
-}
+      [e.target.id]: e.target.value
+    });
+  }
 
-handleSubmit = (e) =>{
+  handleSubmit = (e) => {
     e.preventDefault();
     console.log(this.state);
-}
+  }
 
-/*--------for Member Array function-------- */ 
+  /*--------for Member Array function-------- */
 
-handleChangeArray =(e) => {
+  handleChangeArray = (e) => {
 
-  this.setState({forArray: e.target.value});
-  
-}
+    this.setState({ forArray: e.target.value });
+
+  }
 
 
 
-addPeople = (e) =>{
-  e.preventDefault();
-  const  members = this.state.members;
-  const forArray = this.state.forArray;
-  members.push({name: forArray});
-  this.setState({members:members, forArray: ""});
-  
+  addPeople = (e) => {
+    e.preventDefault();
+    const members = this.state.members;
+    const forArray = this.state.forArray;
+    members.push({ name: forArray });
+    this.setState({ members: members, forArray: "" });
 
-  
-}
-//---------------------------
+
+
+  }
+  //---------------------------
 
   render() {
-    
+
     return (
       <div className="Begin">
 
@@ -62,43 +61,45 @@ addPeople = (e) =>{
         </div>
 
         {/* -----FORM----- */}
-          <div className="TeamInfo">            
-               <form className="teamForm" autoComplete="off" onSubmit={this.handleSubmit}>
-              
-                  <label htmlFor="teamName">Team Name:</label><br/>
-                    <input type="text" id="teamName" placeholder="Enter Team Name" onChange={this.handlechange} required/><br/>
+        <div className="TeamInfo">
+          <form className="teamForm" autoComplete="off" onSubmit={this.handleSubmit}>
 
-                    
-                  <label htmlFor="destination">Destination:</label><br/>
-                    <input type="text" id="destination" placeholder="Enter your Destination" onChange={this.handlechange} required/><br/>
-                  
-                  <label htmlFor="spaceShipName">Spaceship Name:</label><br/>
-                    <input type="text" id="spaceShipName" placeholder="Enter SpaceShip Name" onChange={this.handlechange} required/><br/>
-                  
-                  <label htmlFor="mission">Mission:</label><br/>
-                    <input type="text" id="mission" placeholder="Enter Mission" onChange={this.handlechange} required/><br/>
-                    
-                  <label htmlFor="members"> Members List:</label><br/>
-                    <input type="text" id="members" placeholder="Member Name" onChange={this.handleChangeArray} required/>
-                    {/*-------button for add members to array*/}
-                    <button className="addmem" onClick={this.addPeople}>Add The Member</button> <br/>
-                         {/*----Rendering Member Data------ */}
-                         {
-                      this.state.members.map((data,index) =>{
-                        return(
-                          <div key={index}>
-                            <p>{data.name}</p>
-                          </div>
-                        );
-                      })
-                    }
+            <label htmlFor="teamName">Team Name:</label><br />
+            <input type="text" id="teamName" placeholder="Enter Team Name" onChange={this.handlechange} required /><br />
 
-                    
-                  <button className="btn" >Ready For Take off in 3...2...1...</button>
-                      
-                </form>
-                
-          </div>
+
+            <label htmlFor="destination">Destination:</label><br />
+            <input type="text" id="destination" placeholder="Enter your Destination" onChange={this.handlechange} required /><br />
+
+            <label htmlFor="spaceShipName">Spaceship Name:</label><br />
+            <input type="text" id="spaceShipName" placeholder="Enter SpaceShip Name" onChange={this.handlechange} required /><br />
+
+            <label htmlFor="mission">Mission:</label><br />
+            <input type="text" id="mission" placeholder="Enter Mission" onChange={this.handlechange} required /><br />
+
+            <label htmlFor="members"> Members List:</label><br />
+            <input type="text" id="members" placeholder="Member Name" onChange={this.handleChangeArray} required />
+
+            {/*-------button for add members to array*/}
+            <button className="addmem" onClick={this.addPeople}>Add The Member</button> <br />
+
+            {/*----Rendering Member Data------ */}
+            {
+              this.state.members.map((data, index) => {
+                return (
+                  <div className="teammem" key={index}>
+                      <p>{data.name}</p>
+                  </div>
+                );
+              })
+            }
+
+
+            <button className="btn" >Ready For Take off in 3...2...1...</button>
+
+          </form>
+
+        </div>
 
 
 
