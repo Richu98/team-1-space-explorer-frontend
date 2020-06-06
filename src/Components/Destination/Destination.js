1dc3931a5920f6c2bbee1d7ba8f7654da34da141
Pankaj-Harit <Pankaj-Harit@gmail.com>
2020-06-06 11:48:09 +0530

    add imgData to a pictures array

--- src/Components/Destination/Destination.js
+++ src/Components/Destination/Destination.js
@@ -11,46 +11,73 @@ class Destination extends Component{
     }
 
     state = {
-        pictures: ""
+        imgUrl :'https://meetgreet-upload.herokuapp.com/images/',
+        pictures: []
     }
 
     handleImageSelection = (e) =>{
-        this.setState({ pictures: e.target.files[0]})
+        this.setState({ imgAdd: e.target.files[0]})
     }
 
 
     handleImageUpload = (e) => {
 
+        /*----------for uploading an image--------- */
+
         const image = new FormData();
         const config = {
-            headers: { 'content-type': 'multipart/form-data' }
-        };
-        image.append('file',this.state.pictures,this.state.pictures.name);
+            headers: { 'content-type': 'multipart/form-data' } };
+        console.log(this.state.imgAdd);    
+        image.append('file',this.state.imgAdd,this.state.imgAdd.name);
         axios.post('https://meetgreet-upload.herokuapp.com/upload',image,config).then(res =>{
+            this.setState({
+                imgData: res.data,
+                imgGetUrl: this.state.imgUrl + res.data
+            })
+        })
+        .catch(res =>{
             console.log(res);
         })
+        /*-------To add imgData in into array */
+
+
+        const pictures = this.state.pictures;
+        pictures.push(this.state.imgData);
+        this.setState({pictures: pictures})
+        
+        /*---------------------------------- */
+        console.log(this.state);
+        
+        console.log(this.state.pictures);
+
     }
 
 
 
     handleClick = (e) => {
+        console.log(this.state);
         this.props.history.push("/Return");
+        
     }
 
     
     
     
     render(){
-        console.log(this.state.pictures);
+        
         return(
             <div className="Destination">
                 <div className="imageContainer">
                     <input type="file" onChange = {this.handleImageSelection}/>
                     <button className="upload" onClick = {this.handleImageUpload}> Upload </button>
+                    <div>
+                        
+                        
+                    </div>
 
-                                {/*-------------FOOTER------------- */}
+                    {/*-------------FOOTER------------- */}
 
-                        <button className="butClass" onClick={this.handleClick}>It's Time To Go Home!! </button>        
+                        <button className="butClass" onClick={this.handleClick}>It's Time To Go Home!! :( </button>        
 
 
                 </div>
